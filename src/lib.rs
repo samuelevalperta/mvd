b8e75e549766b9a15bcf8f749f5ebc64e099fe70
Samuele Valperta <samuele.valperta@gmail.com>
2024-07-24 16:31:17 +0200

    Refactor `run` function and add `EntryFilter` trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 pub mod args;
 
 use std::env;
-use std::fs::{self, rename, DirEntry};
-use std::path::{Path, PathBuf};
+use std::fs::{self, copy, rename, DirEntry};
+use std::path::Path;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 use anyhow::{Context, Result};
@@ -11,42 +11,39 @@ use args::Args;
 pub fn run(mut args: Args) -> Result<()> {
     let mut entries = get_file_entries(args.source)?;
 
-    // Remove all the folders from the entries if the relative flag a is not set
-    if !args.include_folders {
-        entries.retain(|entry| entry.metadata().unwrap().is_file())
-    };
+    entries.apply_entry_filter(args.include_folders, args.all_files);
 
-    // Remove all the hidden files from the entries if the relative flag a is not set
-    if !args.all_files {
-        entries.retain(|entry| !entry.file_name().to_str().unwrap().starts_with('.'))
-    };
+    entries.sort_entries_by_creation_time();
 
-    for i in 0..args.n {
-        if let false = args.copy {
-            let entry = match entries.pop() {
-                Some(entry) => entry,
-                None => std::process::exit(0),
-            }
+    println!("{:?}", entries);
 
-            let old_path = entry.path();
-            let original_name = entry.file_name()
-                .to_str()
-                .with_context(|| format!("Failed to read file name: {:?}", old_path))?;
+    for _ in 0..args.n {
+        let entry = match entries.pop() {
+            Some(entry) => entry,
+            None => std::process::exit(0),
+        };
+        let old_path: String = entry.path().to_string_lossy().into_owned();
+        let original_name: String = entry.file_name().to_str().unwrap().to_string();
 
-            let mut new_path = env::var("PWD").unwrap();
+        let mut new_path: String = env::var("PWD").unwrap();
+        new_path.push('/');
 
-            if let Some(new_name) = args.names.pop() {
-                new_path.push_str(&new_name.unwrap())
-            } else {
-                new_path.push_str(&original_name)
-            }
-            
-            rename(old_path, new_path);
+        if let Some(new_name) = args.names.pop() {
+            new_path.push_str(&new_name.unwrap())
+        } else {
+            new_path.push_str(&original_name)
         }
-        
-    }
 
-    sort_entries_by_creation_time(&mut entries);
+        if args.copy {
+            println!("Copying{:?} to {:?}", old_path, new_path);
+            copy(&old_path, &new_path)
+                .with_context(|| format!("Unable to copy {} to {}", old_path, new_path))?;
+        } else {
+            println!("Moving {:?} to {:?}", old_path, new_path);
+            rename(&old_path, &new_path)
+                .with_context(|| format!("Unable to move {} to {}", old_path, new_path))?;
+        }
+    }
 
     for e in entries {
         println!("Path: {:?}", e.path())
@@ -64,15 +61,42 @@ fn get_file_entries<P: AsRef<Path>>(path: P) -> Result<Vec<DirEntry>> {
     Ok(entries)
 }
 
-fn sort_entries_by_creation_time(entries: &mut [DirEntry]) {
-    entries.sort_by_key(|entry| get_creation_time(entry).unwrap_or(UNIX_EPOCH));
-    entries.reverse();
-}
-
 fn get_creation_time(entry: &DirEntry) -> Result<SystemTime> {
     entry
         .metadata()
         .with_context(|| format!("Failed to read metadata for: {:?}", entry.path()))?
         .created()
         .with_context(|| format!("Failed to get creation time for: {:?}", entry.path()))
+}
+
+trait EntryFilter {
+    fn apply_entry_filter(&mut self, include_folders: bool, all_files: bool) -> &mut Self;
+    fn sort_entries_by_creation_time(&mut self) -> &mut Self;
+}
+
+impl EntryFilter for Vec<DirEntry> {
+    fn apply_entry_filter(&mut self, include_folders: bool, all_files: bool) -> &mut Self {
+        // Remove all the folders from the entries if the relative flag a is not set
+        if !include_folders {
+            self.retain(|entry| entry.metadata().unwrap().is_file());
+            println!("Removed folders");
+        } else {
+            todo!();
+        };
+
+        // Remove all the hidden files from the entries if the relative flag a is not set
+        if !all_files {
+            self.retain(|entry| !entry.file_name().to_str().unwrap().starts_with('.'));
+            println!("Remove hidden files");
+        };
+
+        self
+    }
+
+    fn sort_entries_by_creation_time(&mut self) -> &mut Self {
+        self.sort_by_key(|entry| get_creation_time(entry).unwrap_or(UNIX_EPOCH));
+        self.reverse();
+
+        self
+    }
 }
